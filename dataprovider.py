Fix more of the underflow issues when producing csv output

dataprovider.py
@@ -375,7 +375,7 @@ class NumpyCorpusStructure:
                 mean = csv_col_spec.mean
                 sd = csv_col_spec.sd
                 out = ndarray[:, column_slice] * sd + mean
-                if csv_col_spec.min_value > 0:
+                if csv_col_spec.min_value >= 0:
                     out = np.maximum(out, 0)
                 dataframe[csv_col_spec.name] = out
 